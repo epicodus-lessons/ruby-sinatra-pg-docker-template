a4eda12ccb16675051b8fc70c2a424ef2c0a9c93
Brooke Kullberg <48640716+BrookeZK@users.noreply.github.com>
2021-12-06 14:16:25 -0800

    comment out PG.connect database connection

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ also_reload 'lib/**/*.rb'
 require 'pry'
 require "pg"
 
-DB = PG.connect({ dbname: 'record_store', host: 'db', user: 'postgres', password: 'password' })
+# DB = PG.connect({ dbname: 'record_store', host: 'db', user: 'postgres', password: 'password' })
 
 get '/' do
   "This is connected to the database #{DB.db}."
